5f34216b955336364b890e0823e3d363f0f6e638
SedativeHypnotics <sihantawsik@gmail.com>
2020-02-19 10:30:04 +0600

    added things

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,82 @@
+from pytz import utc, timezone
+from datetime import datetime
+from django.utils.translation import ugettext_lazy as _
+
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
 # Create your models here.
+class UserManager(BaseUserManager):
+    def _create_user(
+        self,
+        email,
+        password,
+        full_name,
+        **extra_fields
+    ):
+        if not username:
+            raise ValueError("Users must have an email address")
+        utc_now = utc.localize(datetime.utcnow())
+        now = utc_now.astimezone(timezone("Asia/Dhaka"))
+        user = self.model(
+            email=email,
+            full_name=full_name,
+            last_login=now,
+            is_active=True,
+            date_joined=now,
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(
+        self, email=None, password=None, full_name=None, **extra_fields
+    ):
+        return self._create_user(
+            email, password, full_name, **extra_fields
+        )
+
+    def create_superuser(self, email, password, **extra_fields):
+        user = self._create_user(
+            email, password, **extra_fields
+        )
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    class Meta:
+        db_table = "auth_user"
+
+    email = models.EmailField(_("Email Address"), primary_key=True)
+    registration_number = models.IntegerField(unique=True, null=False)
+    full_name = models.CharField(
+        _("Full Name"),
+        max_length=100,
+        blank=False,
+        null=False,
+        unique=False,
+        default=None,
+    )
+    is_active = models.BooleanField(default=True)
+    last_login = models.DateTimeField(null=True, blank=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+    other = models.Manager()
+
+    def __str__(self):
+        return "username: {}\nemail: {}\n".format(
+            self.username, self.email
+        )
+
+    def get_username(self):
+        return self.username
--- file_io/models.py
+++ file_io/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+from accounts.models import User
 # Create your models here.
+class Article(models.Model):
+    class Meta:
+        db_table = "article"
+    name = models.CharField(max_length=32, null=False, blank=False, unique=True)
+    author = models.ForeignKey(User, to_field="registration_number", null=False, blank=False, on_delete=models.CASCADE)
+    date = models.DateTimeField(["%Y-%m-%d"])
+
+class Tag(models.Model):
+    class Meta:
+        db_table = "tag"
+    name = models.TextField(null=False)
+
+class ArticleTag(models.Model):
+    class Meta:
+        db_table = "article_tag"
+        unique_together = [["article", "tag"],]
+    article = models.OneToOneField(Article, to_field="id", on_delete=models.CASCADE)
+    tag = models.OneToOneField(Tag, to_field="id", on_delete=models.CASCADE)
+
